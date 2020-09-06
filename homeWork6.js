cbcef9db574979263df27b10df4c53fdca05e914
Aleksei <eao173a@gmail.com>
2020-09-06 16:49:22 +0500

    end

--- homeWork6.js
+++ homeWork6.js
@@ -1,7 +1,18 @@
-var onex = document.querySelector('.one');
-var twox = document.querySelector('.two');
-var threex = document.querySelector('.three');
+let oneX = document.querySelector('.one');
+let twoX = document.querySelector('.two');
+let threeX = document.querySelector('.three');
 
-function clickPic() {
-
+function f() {
+    document.querySelector('.big').style.backgroundImage="url('img/big/brit.jpg')";
 }
+function x() {
+    document.querySelector('.big').style.backgroundImage="url('img/big/noname.jpg')";
+}
+function z() {
+    document.querySelector('.big').style.backgroundImage="url('img/big/white.jpg')";
+}
+
+oneX.onclick = f;
+twoX.onclick = x;
+threeX.onclick = z;
+
