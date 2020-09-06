bfb5c797032061a3c3867d71756f000cc45ca41c
Aleksei <eao173a@gmail.com>
2020-09-06 17:09:11 +0500

    23

--- homeWork6.js
+++ homeWork6.js
@@ -3,17 +3,17 @@ let twoX = document.querySelector('.two');
 let threeX = document.querySelector('.three');
 
 function f() {
-    document.querySelector('.big').style.backgroundImage="url('img/big/brit.jpg')";
+    document.querySelector('.big').style.backgroundImage = "url('img/big/brit.jpg')";
 }
 function x() {
-    document.querySelector('.big').style.backgroundImage="url('img/big/noname.jpg')";
+    document.querySelector('.big').style.backgroundImage = "url('img/big/noname.jpg')";
 }
 function z() {
-    document.querySelector('.big').style.backgroundImage="url('img/big/white.jpg')";
+    document.querySelector('.big').style.backgroundImage = "url('img/big/white.jpg')";
 }
 
 oneX.onclick = f;
 twoX.onclick = x;
 threeX.onclick = z;
 
-// Можно сделать решение, через массив.
+// Можно сделать решение, через массив. 3 Здадание, так же через массив. Решить позже.
